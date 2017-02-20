d730071a4531ec340dc480080fc59ba4fe8c8cd9
Eugene Pempel <eugene.pempel@gmail.com>
2017-02-20 11:00:08 +0300

    Add a simple design based on Bootstrap 3.3.7

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,6 +1,6 @@
 class TeamsController < ApplicationController
   get "/team", auth: true do
-    @members = TeamMembersPresenter.new(tid: current_identity.tid).members
+    @report = TeamReportPresenter.new(tid: current_identity.tid)
     slim :team
   end
 end
---
+++ app/presenters/team_report_presenter.rb
@@ -0,0 +1,33 @@
+class TeamReportPresenter
+  attr_reader :tid, :from, :to
+
+  def initialize(tid:, from: nil, to: nil)
+    @tid = tid.to_s
+    @from = from || Time.now.beginning_of_month
+    @to = to || Time.now.end_of_month
+  end
+
+  def members
+    @members ||= build_members.sort_by(&:points).reverse
+  end
+
+  private
+
+  def build_members
+    Identity.where(tid: tid).all.map { |i| build_member(i) }
+  end
+
+  def build_member(identity)
+    points = begin
+      identity.feed.events
+        .where(_type: "Event::PointsEarned")
+        .where(created_at: (from..to))
+        .sum(:points)
+    end
+    OpenStruct.new.tap do |member|
+      member.points = points
+      member.nickname = identity.nickname
+      member.name = identity.name
+    end
+  end
+end

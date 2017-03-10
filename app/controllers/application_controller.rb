3dd38f24bafea9bfc829c638124e3e8686c1712e
Eugene Pempel <eugene.pempel@gmail.com>
2017-03-10 21:01:57 +0300

    Remove permission scopes of the Slack application

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < Sinatra::Base
   use OmniAuth::Builder do
     client_id = ENV["SLACK_CLIENT_ID"]
     client_secret = ENV["SLACK_CLIENT_SECRET"]
-    provider :slack, client_id, client_secret, scope: "commands,users:read"
+    provider :slack, client_id, client_secret
   end
 
   before do
